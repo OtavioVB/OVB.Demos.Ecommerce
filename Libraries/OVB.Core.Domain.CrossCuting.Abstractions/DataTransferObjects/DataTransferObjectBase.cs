eea154df20973ec2bcf3e58c54a47f2001a88902
OtavioVB <otaviovb.developer@gmail.com>
2022-11-25 15:52:14 -0300

    feat: `Infra and Domain Abstractions`

---
+++ Libraries/OVB.Core.Domain.CrossCuting.Abstractions/DataTransferObjects/DataTransferObjectBase.cs
@@ -0,0 +1,21 @@
+﻿using OVB.Core.Domain.CrossCutting.Abstractions.DataTransferObjects;
+
+namespace OVB.Core.Domain.CrossCuting.Abstractions.DataTransferObjects;
+
+public abstract class DataTransferObjectBase : IDataTransferObject
+{
+    public Guid Identifier { get;set; }
+    public DateTime RegisteredOn { get; set; }
+    public Guid RegisteredByIdentifier { get; set; }
+    public DateTime LastModificationOn { get; set; }
+    public Guid LastModificationByIdentifier { get; set; }
+
+    protected DataTransferObjectBase(Guid identifier, DateTime registeredOn, Guid registeredByIdentifier, DateTime lastModificationOn, Guid lastModificationByIdentifier)
+    {
+        Identifier = identifier;
+        RegisteredOn = registeredOn;
+        RegisteredByIdentifier = registeredByIdentifier;
+        LastModificationOn = lastModificationOn;
+        LastModificationByIdentifier = lastModificationByIdentifier;
+    }
+}
---
+++ Libraries/OVB.Core.Infrascructure.CrossCutting.Abstractions/Mappings/BaseMapping.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OVB.Core.Domain.CrossCuting.Abstractions.DataTransferObjects;
+using OVB.Core.Domain.CrossCutting.Abstractions.DataTransferObjects;
+
+namespace OVB.Core.Infrascructure.CrossCutting.Abstractions.Mappings;
+
+public abstract class BaseMapping<T> where T : DataTransferObjectBase
+{
+    public void CreateMapping(EntityTypeBuilder<T> builder)
+    {
+        builder.HasKey(p => p.Identifier);
+
+        MappingOverrideExtensions(builder);
+    }
+
+    public virtual void MappingOverrideExtensions(EntityTypeBuilder<T> builder)
+    {
+        return;
+    }
+}
