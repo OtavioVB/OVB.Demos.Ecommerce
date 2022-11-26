d1a44aa40580a484c8a8dff90a80daa04866bfce
OtavioVB <otaviovb.developer@gmail.com>
2022-11-26 19:38:08 -0300

    feat: `Create Authentication Account Input`

---
+++ Microsservices/OVB.Demos.Ecommerce.Microsservices.Account.Services/Handlers/CreateAccount/Inputs/CreateAccountInput.cs
@@ -0,0 +1,25 @@
+﻿using OVB.Demos.Ecommerce.Microsservices.Account.Domain.Common.Models.Properties;
+
+namespace OVB.Demos.Ecommerce.Microsservices.Account.Services.Handlers.CreateAccount.Inputs;
+
+public class CreateAccountInput : IAccountProperties
+{
+    public string Username { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public string Office { get; set; }
+    public string CPF { get; set; }
+
+    public CreateAccountInput(string username, string name, string surname, string password, string email, string office, string cpf)
+    {
+        Username = username;
+        Name = name;
+        Surname = surname;
+        Password = password;
+        Email = email;
+        Office = office;
+        CPF = cpf;
+    }
+}
