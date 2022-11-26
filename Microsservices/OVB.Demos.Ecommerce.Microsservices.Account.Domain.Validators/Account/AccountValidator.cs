2aa9f5258d7bcfaae3af7b7c4bf0c4b6f15be361
OtavioVB <otaviovb.developer@gmail.com>
2022-11-26 12:24:49 -0300

    feat: `Validators`

---
+++ Microsservices/OVB.Demos.Ecommerce.Microsservices.Account.Domain.Validators/Account/AccountValidator.cs
@@ -0,0 +1,17 @@
+﻿using OVB.Core.CrossCutting.Validator.Abstractions;
+using OVB.Demos.Ecommerce.Microsservices.Account.Domain.Common.Models.Properties;
+
+namespace OVB.Demos.Ecommerce.Microsservices.Account.Domain.Validators.Account;
+
+public class AccountValidator : EcommerceOwnValidatorBase<IAccountGettersProperties, ValidationErrorItemStandard>
+{
+    protected override ValidationErrorItemStandard InvalidNullInput()
+    {
+        return new ValidationErrorItemStandard("Os dados inseridos são inválidos!");
+    }
+
+    public override void ValidationWorkflow(IAccountGettersProperties property)
+    {
+        base.ValidationWorkflow(property);
+    }
+}
---
+++ Microsservices/OVB.Demos.Ecommerce.Microsservices.Account.Domain.Validators/ValidationErrorItemStandard.cs
@@ -0,0 +1,10 @@
+﻿using OVB.Core.CrossCutting.Validator.Abstractions.ValidationErrorItem;
+
+namespace OVB.Demos.Ecommerce.Microsservices.Account.Domain.Validators;
+
+public class ValidationErrorItemStandard : ValidationErrorBase
+{
+    public ValidationErrorItemStandard(string message) : base(message)
+    {
+    }
+}
