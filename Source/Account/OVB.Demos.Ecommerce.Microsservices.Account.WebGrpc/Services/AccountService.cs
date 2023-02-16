ad34500711c8676cb98bf4451f2a82f46706ebb3
OtavioVB <otaviovb.developer@gmail.com>
2023-02-16 19:03:22 -0300

    refact: `Protobuf Model`

--- Source/Account/OVB.Demos.Ecommerce.Microsservices.Account.WebGrpc/Services/AccountService.cs
+++ Source/Account/OVB.Demos.Ecommerce.Microsservices.Account.WebGrpc/Services/AccountService.cs
@@ -1,12 +1,16 @@
 using Grpc.Core;
-using OVB.Demos.Ecommerce.Microsservices.Account.WebGrpc;
 
 namespace OVB.Demos.Ecommerce.Microsservices.Account.WebGrpc.Services;
 
-public class AccountService : Account.AccountBase
+public sealed class AccountService : Account.AccountBase
 {
     public override Task<CreateAccountUseCaseOutput> CreateAccount(CreateAccountUseCaseInput request, ServerCallContext context)
     {
         return base.CreateAccount(request, context);
+    }
+
+    public override Task<HealthCheckOutput> HealthCheck(HealthCheckInput request, ServerCallContext context)
+    {
+        return base.HealthCheck(request, context);
     }
 }
