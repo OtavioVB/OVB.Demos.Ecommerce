7bbae61e48786861820707323462aac8b7c2f5f4
OtavioVB <otaviovb.developer@gmail.com>
2023-02-18 12:37:45 -0300

    refact: `gRPC endpoint service`

---
+++ Source/Account/OVB.Demos.Ecommerce.Microsservices.Account.WebGrpc/Adapters/AdapterCreateAccountUseCaseGrpcInputToCreateAccountUseCaseInput.cs
@@ -0,0 +1,21 @@
+﻿using OVB.Demos.Ecommerce.Microsservices.Account.Application.Services.UseCases.Inputs;
+using OVB.Demos.Ecommerce.Microsservices.Base.DesignPatterns.Adapter;
+
+namespace OVB.Demos.Ecommerce.Microsservices.Account.WebGrpc.Adapters;
+
+public class AdapterCreateAccountUseCaseGrpcInputToCreateAccountUseCaseInput : IAdapter<CreateAccountUseCaseGrpcInput, CreateAccountUseCaseInput>
+{
+    public CreateAccountUseCaseInput Adapter(CreateAccountUseCaseGrpcInput input)
+    {
+        return new CreateAccountUseCaseInput(
+            new Guid(input.TenantIdentifier), 
+            new Guid(input.CorrelationIdentifier),
+            input.SourcePlatform,
+            input.ExecutionSource, 
+            input.Name, 
+            input.LastName, 
+            input.Username, 
+            input.Password, 
+            input.Email);
+    }
+}
--- Source/Account/OVB.Demos.Ecommerce.Microsservices.Account.WebGrpc/Services/AccountService.cs
+++ Source/Account/OVB.Demos.Ecommerce.Microsservices.Account.WebGrpc/Services/AccountService.cs
@@ -1,22 +1,34 @@
 using Grpc.Core;
+using OVB.Demos.Ecommerce.Microsservices.Account.Application.Services.UseCases.Inputs;
+using OVB.Demos.Ecommerce.Microsservices.Account.Application.Services.UseCases.Interfaces;
+using OVB.Demos.Ecommerce.Microsservices.Base.DesignPatterns.Adapter;
 
 namespace OVB.Demos.Ecommerce.Microsservices.Account.WebGrpc.Services;
 
 public sealed class AccountService : Account.AccountBase
 {
-    public override Task<CreateAccountUseCaseOutput> CreateAccount(CreateAccountUseCaseInput request, ServerCallContext context)
-    {
-        var output = new CreateAccountUseCaseOutput()
-        {
-            Created = true,
-        };
+    private readonly IUseCase<CreateAccountUseCaseInput> _useCaseCreateAccount;
+    private readonly IAdapter<CreateAccountUseCaseGrpcInput, CreateAccountUseCaseInput> _adapterInput;
+    private readonly CancellationToken _cancellationToken;
 
-        output.Messages.Add(new Notification()
+    public AccountService(
+        IUseCase<CreateAccountUseCaseInput> useCaseCreateAccount, 
+        IAdapter<CreateAccountUseCaseGrpcInput, CreateAccountUseCaseInput> adapterInput, 
+        CancellationToken cancellationToken)
+    {
+        _useCaseCreateAccount = useCaseCreateAccount;
+        _adapterInput = adapterInput;
+        _cancellationToken = cancellationToken;
+    }
+
+    public override async Task<CreateAccountUseCaseGrpcOutput> CreateAccount(
+        CreateAccountUseCaseGrpcInput request, ServerCallContext context)
+    {
+        var useCaseResponse = await _useCaseCreateAccount.ExecuteUseCaseAsync(_adapterInput.Adapter(request), _cancellationToken);
+        return new CreateAccountUseCaseGrpcOutput()
         {
-            Message = "Teste",
-            TypeNotification = "Teste",
-        });
-        return Task.FromResult(output);
+            Created = true
+        };
     }
 
     public override Task<HealthCheckOutput> HealthCheck(HealthCheckInput request, ServerCallContext context)
