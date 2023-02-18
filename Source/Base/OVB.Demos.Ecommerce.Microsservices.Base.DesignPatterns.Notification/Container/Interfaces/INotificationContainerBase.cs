3bbbfa348e8f594d681bcad7e0073eee4c5a7c2b
OtavioVB <otaviovb.developer@gmail.com>
2023-02-18 12:47:24 -0300

    feat: `Notification Container`

---
+++ Source/Base/OVB.Demos.Ecommerce.Microsservices.Base.DesignPatterns.Notification/Container/Interfaces/INotificationContainerBase.cs
@@ -0,0 +1,11 @@
+﻿using OVB.Demos.Ecommerce.Microsservices.Base.DesignPatterns.Notification.Item.Interfaces;
+
+namespace OVB.Demos.Ecommerce.Microsservices.Base.DesignPatterns.Notification.Container.Interfaces;
+
+public interface INotificationContainerBase<TNotification>
+    where TNotification : INotificationItem
+{
+    public Task AddNotification(TNotification notification);
+    public Task AddNotifications(IEnumerable<TNotification> notifications);
+    public Task<IList<TNotification>> GetNotifications();
+}
