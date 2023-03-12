fb7e53c1ef9dc4ffb0ea5544b3375164276a183c
OtavioVB <otaviovb.developer@gmail.com>
2023-03-12 17:37:24 -0300

    feat: `abstraction for repositories`

---
+++ src/Account/OVB.Demos.Ecommerce.Microsservices.AccountManagement.Infrascructure/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OVB.Demos.Ecommerce.Libraries.Domain;
+using OVB.Demos.Ecommerce.Microsservices.AccountManagement.Infrascructure.Repositories.Interfaces;
+
+namespace OVB.Demos.Ecommerce.Microsservices.AccountManagement.Infrascructure.Repositories.Base;
+
+public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity>
+    where TEntity : DataTransferObjectBase
+{
+    protected readonly DataContext _dataContext;
+
+    protected BaseRepository(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    public Task AddAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(_dataContext.Set<TEntity>().AddAsync(entity, cancellationToken));
+    }
+
+    public Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(_dataContext.Set<TEntity>().AddRangeAsync(entities, cancellationToken));
+    }
+
+    public Task DeleteAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(() =>
+        {
+            _dataContext.Set<TEntity>().Remove(entity);
+        });
+    }
+
+    public Task DeleteRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(() =>
+        {
+            _dataContext.Set<TEntity>().RemoveRange(entities);
+        });
+    }
+
+    public Task<TEntity?> GetByIdentifierAsync(Guid identifier, CancellationToken cancellationToken)
+    {
+        return _dataContext.Set<TEntity>().Where(p => p.Identifier == identifier).FirstOrDefaultAsync();
+    }
+
+    public Task UpdateAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(() =>
+        {
+            _dataContext.Set<TEntity>().Update(entity);
+        });
+    }
+
+    public Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(() =>
+        {
+            _dataContext.Set<TEntity>().UpdateRange(entities);
+        });
+    }
+}
---
+++ src/Account/OVB.Demos.Ecommerce.Microsservices.AccountManagement.Infrascructure/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using OVB.Demos.Ecommerce.Libraries.Domain;
+
+namespace OVB.Demos.Ecommerce.Microsservices.AccountManagement.Infrascructure.Repositories.Interfaces;
+
+public interface IBaseRepository<TEntity>
+    where TEntity : DataTransferObjectBase
+{
+    public Task AddAsync(TEntity entity, CancellationToken cancellationToken);
+    public Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken);
+    public Task UpdateAsync(TEntity entity, CancellationToken cancellationToken);
+    public Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken);
+    public Task DeleteAsync(TEntity entity, CancellationToken cancellationToken);
+    public Task DeleteRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken);
+    public Task<TEntity?> GetByIdentifierAsync(Guid identifier, CancellationToken cancellationToken);
+}
