c79052b2b96bc68910e21f523c1109dd3f1e83de
OtavioVB <otaviovb.developer@gmail.com>
2023-03-17 19:03:51 -0300

    fix: `error 404`

--- frontend/src/routes/NotFound/NotFound.js
+++ frontend/src/routes/NotFound/NotFound.js
@@ -1,5 +1,12 @@
 import './NotFound.css';
+import { Link } from 'react-router-dom';
 
 export default function NotFound(){
-    return (<p>ERROR 404</p>);
+    return (
+            <main class="notfound-main">
+                <h1 className='notfound-error-title'>ERROR 404</h1>
+                <p className='notfound-error-description'>Não foi possível encontrar a página esperada</p>
+                <Link className='notfound-component-redirect-other-link' to="/">Voltar para página inicial</Link>
+            </main>
+        );
 }
