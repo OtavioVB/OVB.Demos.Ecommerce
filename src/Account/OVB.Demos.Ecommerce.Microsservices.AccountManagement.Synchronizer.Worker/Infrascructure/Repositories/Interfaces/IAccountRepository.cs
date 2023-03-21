8a4b313dd2c3ebabbd552bf4d49100825359d524
OtavioVB <otaviovb.developer@gmail.com>
2023-03-20 23:11:44 -0300

    feat: `account repository for worker synchronizer`

---
+++ src/Account/OVB.Demos.Ecommerce.Microsservices.AccountManagement.Synchronizer.Worker/Infrascructure/Repositories/Interfaces/IAccountRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace OVB.Demos.Ecommerce.Microsservices.AccountManagement.Synchronizer.Worker.Infrascructure.Repositories.Interfaces;
+
+public interface IAccountRepository
+{
+    public Task CreateTableAccountIfThisNotExists();
+    public Task AddAccountAsync();
+}
