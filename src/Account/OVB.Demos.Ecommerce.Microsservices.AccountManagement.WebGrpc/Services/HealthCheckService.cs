9e6814942fa7e4222a4874106810b3338896e364
OtavioVB <otaviovb.developer@gmail.com>
2023-03-20 22:15:10 -0300

    feat: `readiness microsservice account endpoint`

--- src/Account/OVB.Demos.Ecommerce.Microsservices.AccountManagement.WebGrpc/Services/HealthCheckService.cs
+++ src/Account/OVB.Demos.Ecommerce.Microsservices.AccountManagement.WebGrpc/Services/HealthCheckService.cs
@@ -1,11 +1,57 @@
 ﻿using Grpc.Core;
+using OVB.Demos.Ecommerce.Libraries.Infrascructure.HealthChecks.ENUMs;
+using OVB.Demos.Ecommerce.Libraries.Infrascructure.HealthChecks.Interfaces.Inputs.Interfaces;
+using OVB.Demos.Ecommerce.Microsservices.AccountManagement.Infrascructure.HealthChecks.Interfaces;
 
 namespace OVB.Demos.Ecommerce.Microsservices.AccountManagement.WebGrpc.Services;
 
 public sealed class HealthCheckService : HealthChecks.HealthChecksBase
 {
-    public override Task<CreateAccountOutput> ReadinessHealthCheck(ReadinessHealthCheckInput request, ServerCallContext context)
+    private readonly IDatabaseHealthCheck _databaseHealthCheck;
+
+    public HealthCheckService(IDatabaseHealthCheck databaseHealthCheck)
     {
-        return base.ReadinessHealthCheck(request, context);
+        _databaseHealthCheck = databaseHealthCheck;
+    }
+
+    public override async Task<ReadinessHealthCheckOutput> ReadinessHealthCheck(ReadinessHealthCheckInput request, ServerCallContext context)
+    {
+        bool hasAnyUnhealthy = false;
+
+        var postgreeHealthCheck = await _databaseHealthCheck.ReadinessHealthCheck(context.CancellationToken);
+        if (postgreeHealthCheck.Status == Libraries.Infrascructure.HealthChecks.ENUMs.HealthCheckStatus.Unhealthy)
+            hasAnyUnhealthy = true;
+
+
+        var response = new ReadinessHealthCheckOutput();
+        response.Services.AddRange(Convert(postgreeHealthCheck));
+        if (hasAnyUnhealthy == true)
+        {
+            response.Ready = HealthCheckStatus.Unhealthy.ToString();
+            return response;
+        }
+        else
+        {
+            response.Ready = HealthCheckStatus.Healthy.ToString();
+            return response;
+        }
+    }
+
+    private IEnumerable<ServiceReadiness> Convert(params IHealthCheckServiceStatus[] status)
+    {
+        var services = new List<ServiceReadiness>();
+
+        foreach (var statu in status)
+        {
+            services.Add(new ServiceReadiness()
+            {
+                ServiceDescription = statu.Description,
+                ServiceIsReady = statu.Status.ToString(),
+                ServiceName = statu.Name,
+                ServiceVersion = statu.Version,
+            });
+        }
+
+        return services;
     }
 }
