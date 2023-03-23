15a3e76a53aff0752f20f5a238026922f5d9a8de
OtavioVB <otaviovb.developer@gmail.com>
2023-03-23 19:16:14 -0300

    feat: `user protobuffer`

---
+++ src/Account/OVB.Demos.Ecommerce.Microsservices.AccountManagement.Domain/UserContext/Protobuffer/AccountProtobuffer.cs
@@ -0,0 +1,39 @@
+﻿using ProtoBuf;
+
+namespace OVB.Demos.Ecommerce.Microsservices.AccountManagement.Domain.UserContext.Protobuffer;
+
+[ProtoContract()]
+public sealed class AccountProtobuffer
+{
+    public AccountProtobuffer(string username, string name, string lastName, string email, string password, int typeUser, bool isEmailConfirmed)
+    {
+        Username = username;
+        Name = name;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+        TypeUser = typeUser;
+        IsEmailConfirmed = isEmailConfirmed;
+    }
+
+    [ProtoMember(1)]
+    public string Username { get; set; }
+
+    [ProtoMember(2)]
+    public string Name { get; set; }
+
+    [ProtoMember(3)]
+    public string LastName { get; set; }
+
+    [ProtoMember(4)]
+    public string Email { get; set; }
+
+    [ProtoMember(5)]
+    public string Password { get; set; }
+
+    [ProtoMember(6)]
+    public int TypeUser { get; set; }
+
+    [ProtoMember(7)]
+    public bool IsEmailConfirmed { get; set; }
+}
